b49d0310581a3952b7268db1f38356e77b212dff
EnriqueGR123 <118323832+EnriqueGR123@users.noreply.github.com>
2024-06-02 09:16:42 -0600

    Inicio
    cachinchinchin

---
+++ main.js
@@ -0,0 +1,7 @@
+const title= document.querySelector(".title");
+const main_div=document.querySelector(".main-div");
+const old_div = document.querySelector(".old-div");
+const list =document.querySelectorAll("ul.list > li")
+const img = document.querySelector("img")
+img.setAttribute("src", "e.png")
+img.classList.add("img")
